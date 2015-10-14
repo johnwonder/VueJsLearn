e113fb10b2b32390c7c2f877a2b7960eec572898
johnwonder <johnufida@163.com>
2015-10-14 17:17:36 +0800

    处理 require('vue-strap').alert的问题

--- vue-strap1/src/index.js
+++ vue-strap1/src/index.js
@@ -4,24 +4,30 @@ import './assets/docs.css'
 import './assets/style.css'
 import 'prismjs'
 
-
+//var vuestrap = require('vue-strap');
 import gettingStarted from './example/gettingStarted.vue'
-import affix from 'vue-strap/src/Affix.vue'
+
+//import { affix } from 'vue-strap'
+//import affix from 'vue-strap/src/Affix.vue'
 import container from './example/container.vue'
 
 //在alertDocs.vue内部 又import了 alert.vue
-import alertDocs from './example/alertDocs.vue'
+//import alertDocs from './example/alertDocs.vue'
 //import accordionDocs from './example/accordionDocs.vue'
 import affixDocs from './example/affixDocs.vue'
 //import alert from 'vue-strap/src/alert'
+//var affix = require('vue-strap/src/affix')
+var alert =  require('vue-strap').alert//这种用法需先找到入口文件 main.js 然后 编译但是 抱vue  this._init is not a function 说明没初始化
+//不能编译包中的vue 把node_modules中的vue删掉就好了
+//var alert =  require('vue-strap/dist/vue-strap').alert//这种已经用了编译好的
 //var alert = require('vue-strap/src/alert')
-//var alert = require('vue-strap').alert
+
+
 new Vue({
   el: '#wrapper',
   components: {
-    affix,
 	container,
-	alertDocs,
-	affixDocs
+	affixDocs,
+	alert
   }
 });
